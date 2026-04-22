6eb127cc3787d3d037b78115f618266f3c6bd94f
barry <barryshen777@gmail.com>
2026-04-22 09:12:34 +0800

    Changed BaseURL to AP-Cyber-Curriculum

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://BarryTheShen.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ap-cyber-curriculum',
+  baseUrl: '/AP-Cyber-Curriculum',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
